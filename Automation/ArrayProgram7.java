98fe99e11b6c06d292380228f275ea8540db8065
shivamt9977 <shivam31t@gmail.com>
2022-05-20 23:40:26 +0530

    ArrayPrograms

---
+++ Automation/ArrayProgram7.java
@@ -0,0 +1,18 @@
+package com.ArrayPrograms;
+
+public class ArrayProgram7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s="Hello java";
+		String[] s1=s.split(" ");
+		for(String s2:s1)
+		{
+			System.out.println(s2);
+		}
+	}
+
+}
+
+
---
+++ Automation/ArrayProgram8.java
@@ -0,0 +1,16 @@
+package com.ArrayPrograms;
+
+public class ArrayProgram8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Hello java";
+		String[] s1=s.split("l");
+		for(String s2:s1)
+		{
+			System.out.println(s2);
+		}
+
+	}
+
+}
